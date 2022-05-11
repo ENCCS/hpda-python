284abb0fc686fddf08ed01a80240dc45b7b7a648
Qiang Li <qiangli19820822@gmail.com>
2022-05-11 11:52:09 +0200

    small fixes

---
+++ content/exercise/lap2d_cuda_benchmark.py
@@ -0,0 +1,6 @@
+# Benchmark properly
+%%timeit 
+d_u = numba.cuda.to_device(u)
+d_unew = numba.cuda.to_device(unew)
+lap2d_cuda[(16,16),(16,16)](d_u, d_unew);numba.cuda.synchronize()
+d_unew.copy_to_host(unew)
