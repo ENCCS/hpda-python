626322fe32adb31ab4ce916a2f55a72a7ed1df5b
Qiang Li <qiangli19820822@gmail.com>
2025-01-20 09:55:38 +0100

    adding missing files

---
+++ content/example/cpu_bound.py
@@ -0,0 +1,22 @@
+from concurrent.futures import ThreadPoolExecutor, ProcessPoolExecutor
+import math
+
+def func_cpu_bound(n):
+    sum = 0
+    for i in range(1, n+1):
+        sum += math.exp(math.log(math.sqrt(math.pow(i, 3.0))))
+    return sum
+
+
+n=1000000
+
+%%time
+func_cpu_bound(n)
+
+%%time
+with ThreadPoolExecutor(max_worker=4) as pool:
+    pool.map(func_cpu_bound, [n,n,n,n])
+
+%%time
+with ProcessPoolExecutor(max_worker=4) as pool:
+    pool.map(func_cpu_bound, [n,n,n,n])
