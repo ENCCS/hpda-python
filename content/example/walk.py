73cef48f6e69ff851ccb0894f76c3c23313407cd
Ashwin V. Mohanan <ashwin.mohanan@enccs.se>
2025-01-21 21:18:04 +0100

    New vectorized walk function

--- content/example/walk.py
+++ content/example/walk.py
@@ -1,20 +1,56 @@
+"""A 1-D random walk.
+
+See also:
+- https://lectures.scientific-python.org/intro/numpy/auto_examples/plot_randomwalk.html
+
+"""
 import numpy as np
+
 
 def step():
     import random
-    return 1. if random.random() > .5 else -1.
+    return 1.0 if random.random() > 0.5 else -1.0
 
-def walk(n):
-    x = np.zeros(n)
-    dx = 1. / n
+
+def walk(n: int, dx: float = 1.0):
+    """The for-loop version.
+
+    Parameters
+    ----------
+    n: int
+        Number of time steps
+
+    dx: float
+        Step size. Default step size is unity.
+
+    """
+    xs = np.zeros(n)
+
     for i in range(n - 1):
-        x_new = x[i] + dx * step()
-        if x_new > 5e-3:
-            x[i + 1] = 0.
-        else:
-            x[i + 1] = x_new
-    return x
+        x_new = xs[i] + dx * step()
+        xs[i + 1] = x_new
+
+    return xs
+
+
+def walk_vec(n: int, dx: float = 1.0):
+    """The vectorized version of :func:`walk` using numpy functions."""
+    import random
+    steps = np.array(random.sample([1, -1], k=n, counts=[10 * n, 10 * n]))
+
+    # steps = np.random.choice([1, -1], size=n)
+
+    dx_steps = dx * steps
+
+    # set initial condition to zero
+    dx_steps[0] = 0
+    # use cumulative sum to replicate time evolution of position x
+    xs = np.cumsum(dx_steps)
+
+    return xs
+
 
 if __name__ == "__main__":
-    n = 100000
-    x = walk(n)
+    n = 1_000_000
+    _ = walk(n)
+    _ = walk_vec(n)
