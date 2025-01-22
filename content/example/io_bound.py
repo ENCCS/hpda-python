43836b297501c38d2bf8234b510c41941ee9c9cb
qianglise <21262137+qianglise@users.noreply.github.com>
2025-01-22 10:01:13 +0100

    Update io_bound.py
    fixing miss spelling

--- content/example/io_bound.py
+++ content/example/io_bound.py
@@ -6,10 +6,10 @@ def func_io_bound(n):
     return
 
 %%time
-with ThreadPoolExecutor(max_worker=4) as pool:
+with ThreadPoolExecutor(max_workers=4) as pool:
     pool.map(func_io_bound, [1,3,1,7])
 
 
 %%time
-with ProcessPoolExecutor(max_worker=4) as pool:
+with ProcessPoolExecutor(max_workers=4) as pool:
     pool.map(func_io_bound, [1,3,1,7])
